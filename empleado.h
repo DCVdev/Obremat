a10291f50f34bc74316aeae22aab884abc2a3495
DCVdev <villa5041@gmail.com>
2024-02-14 19:49:12 +0100

    pruebas con qt y una clase empleado

---
+++ empleado.h
@@ -0,0 +1,14 @@
+#ifndef EMPLEADO_H
+#define EMPLEADO_H
+
+#include <string>
+#include <iostream>
+//Creo la estructura del empleado
+struct Employee{
+    int id;
+    std::string user;
+    std::string password;
+};
+//Creo la función que añade el empleado
+void add_employee(Employee &emp, int id, std::string &user, std::string &password);
+#endif // EMPLEADO_H
